4c5fe73447ecc214cbd8184c2f78f8562216273f
heiNina <nina.shabrova2001@gmail.com>
2020-12-29 09:22:50 +0300

    Fixes

--- 02-directional-bias/js/entry.js
+++ 02-directional-bias/js/entry.js
@@ -1,38 +1,38 @@
-import DLA from '../../core/DLA';
+import DLA from '../../core/DLA'
 
 const sketch = function (p5) {
     const dla = new DLA(p5, {p: 0.6})
 
     // Setup ---------------------------------------------------------------
     p5.setup = function () {
-        p5.createCanvas(window.innerWidth, window.innerHeight);
-        p5.colorMode(p5.HSB, 255);
-        p5.ellipseMode(p5.CENTER);
+        p5.createCanvas(window.innerWidth, window.innerHeight)
+        p5.colorMode(p5.HSB, 255)
+        p5.ellipseMode(p5.CENTER)
 
-        dla.settings.BiasTowards = 'Meridian';
+        dla.settings.BiasTowards = 'Meridian'
 
-        reset();
+        reset()
     }
 
     p5.draw = function () {
-        dla.iterate();
-        dla.draw();
+        dla.iterate()
+        dla.draw()
     }
 
     function reset() {
-        dla.removeAll();
-        dla.createDefaultWalkers();
-        dla.createDefaultClusters();
+        dla.removeAll()
+        dla.createDefaultWalkers()
+        dla.createDefaultClusters()
     }
 
 
     p5.keyReleased = function () {
         if (p5.key === 'r')
-            reset();
+            reset()
         else if (p5.key === 'e')
-            dla.export();
+            dla.export()
 
     }
 }
 
-new p5(sketch);
+new p5(sketch)
